d59768dcebb29b615b362d392bb46cb655aa07b4
Ecaterina Patkany <ecaterina.patkany05@gmail.com>
2026-01-16 09:05:11 +0200

    feat: add generate boilerplate command and editor access

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,22 @@
 // The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+// This method is called the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	const disposable = vscode.commands.registerCommand('c-boilerplate.generate', () => {
+		// The code placed here will be executed every time your command is executed
+		const editor = vscode.window.activeTextEditor;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "c-boilerplate-generator" is now active!');
+		if ( !editor ) {
+			vscode.window.showErrorMessage('No active editor!');
+			return;
+		}
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('c-boilerplate-generator.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from c-boilerplate-generator!');
+		vscode.window.showInformationMessage(`Active file: ${editor.document.fileName}`);
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
+// This method is called when the extension is deactivated
 export function deactivate() {}
